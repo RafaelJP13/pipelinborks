9f6f90676d52e51f431d3631dd6df2dff20658be
Rafael Fernandes <rafaelsf013@gmail.com>
2025-11-09 09:27:22 -0300

    feat: Transform raw data to business logic significante data.

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,16 @@
-from extract import fetch_data
-import json
+from transform import transform_raw_data, save_processed_data
+from pathlib import Path
 
 def main():
-    api_url = "https://api.fda.gov/drug/drugsfda.json"
-    limit = 10
-    data = fetch_data(api_url, limit)
+    raw_dir = Path(__file__).parent / "data" / "raw"
+    raw_file = raw_dir / "drugsfda_08112025153540.json"
 
+    if not raw_file.exists():
+        print("No raw data found")
+        return
 
-    if data:
-        print("Extracted data successfully.")
+    cleaned_data = transform_raw_data(raw_file)
+    save_processed_data(cleaned_data, "processed_drugsfda_08112025153540.json")
 
 if __name__ == "__main__":
     main()
---
+++ src/transform.py
@@ -0,0 +1,38 @@
+import json
+import pandas as pd
+from pathlib import Path
+
+def transform_raw_data(raw_file_path: Path):
+
+    """Transforms raw data from API and get important fields"""
+
+    with open(raw_file_path, "r", encoding = "utf-8") as raw_file:
+        raw_data = json.load(raw_file)
+
+    cleaned_raw_data = []
+
+    for item in raw_data.get("results", []):
+        sponsor_name = item.get("sponsor_name")
+
+        products = item.get("products", [])
+        product_name = products[0].get("brand_name") if products else None
+
+        cleaned_raw_data.append({
+            "sponsor_name": sponsor_name,
+            "product_name": product_name,
+
+        })
+
+    return cleaned_raw_data
+
+def save_processed_data(data, output_name="processed_data.json"):
+    """Saves processed data to Json file"""
+    processed_dir = Path(__file__).parent / "data" / "processed"
+    processed_dir.mkdir(parents=True, exist_ok=True)
+
+    output_path = processed_dir / output_name
+
+    with open(output_path, "w", encoding = "utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+    print(f"Saved processed data to {output_path}")
