73d6e8b2a013d2c809486b7712043c8031e539be
Rafael Fernandes <rafaelsf013@gmail.com>
2025-11-12 10:28:45 -0300

    fix: Untrack .env and get the path to use env credentials.

--- src/tests/test_connection.py
+++ src/tests/test_connection.py
@@ -2,10 +2,13 @@ from dotenv import load_dotenv
 import os
 import mysql.connector
 from mysql.connector import Error
+from pathlib import Path
 
-load_dotenv()
+env_path = Path(__file__).resolve().parents[2] / '.env'
+load_dotenv(dotenv_path=env_path)
 
 def test_connection():
+
     connection = None
     try:
         connection = mysql.connector.connect(
